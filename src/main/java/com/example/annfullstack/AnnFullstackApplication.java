357deabe8d8500b11a3a2f97fd2f6690a15e2c75
= <=>
2024-02-09 01:37:39 +0200

    final

--- src/main/java/com/example/annfullstack/AnnFullstackApplication.java
+++ src/main/java/com/example/annfullstack/AnnFullstackApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AnnFullstackApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(AnnFullstackApplication.class, args);
     }
